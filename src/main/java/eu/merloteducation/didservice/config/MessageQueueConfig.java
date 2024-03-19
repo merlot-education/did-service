85ed9daac4a1da92a5b6aed29f2b4f181d7e02fc
Marc B <125284318+M-Busk@users.noreply.github.com>
2024-03-19 16:09:26 +0100

    add converter for incoming messages

--- src/main/java/eu/merloteducation/didservice/config/MessageQueueConfig.java
+++ src/main/java/eu/merloteducation/didservice/config/MessageQueueConfig.java
@@ -1,9 +1,10 @@
 package eu.merloteducation.didservice.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -32,5 +33,17 @@ public class MessageQueueConfig {
     public Queue didPrivateKeyRequestedQueue() {
 
         return new Queue(DID_PRIVATE_KEY_REQUEST_QUEUE, false);
+    }
+
+    @Bean
+    public MessageConverter converter(){
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(converter());
+        return rabbitTemplate;
     }
 }
